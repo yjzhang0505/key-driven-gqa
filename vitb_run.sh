c73ba95deb08e6c2d5bb41a91100406bdae7a145
yjzhang0505 <zzyyjj0505@163.com>
2024-12-25 10:38:51 +0800

    vitb-3.0

--- vitb_run.sh
+++ vitb_run.sh
@@ -4,8 +4,8 @@
 base_path="/data/yjzhang/desktop/try/not_share/key-driven-gqa/output/dustbin2/"
 
 # 要遍历的文件夹名称数组
-folders=("V_cosine_K_singular" "V_cosine_KxQ_singular" "V_cosine_Q_singular" "V_cosine_V_singular"
-        "V_cosine_K_var" "V_cosine_KxQ_var" "V_cosine_Q_var" "V_cosine_V_var")  # 在这里添加其他文件夹名称
+# folders=("gqa")  # 在这里添加其他文件夹名称
+folders=("V_cosine_V_singular" "V_cosine_V_var")  # 在这里添加其他文件夹名称
 
 # 遍历每个文件夹
 for folder in "${folders[@]}"
@@ -20,6 +20,11 @@ do
     # 执行命令
     $command
 done
+
+gqa_command="python /data/yjzhang/desktop/try/not_share/key-driven-gqa/train_freeze_gqa.py"
+echo "Running final command: $gqa_command"
+$gqa_command
+
 
 # chmod +x vitb_run.sh
 # ./vitb_run.sh
